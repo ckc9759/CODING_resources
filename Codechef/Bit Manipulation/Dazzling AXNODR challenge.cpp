d23f40d36c3cda7d0c39822e227ed3f958645516
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-07-09 16:49:49 +0530

    Create Dazzling AXNODR challenge.cpp

---
+++ Codechef/Bit Manipulation/Dazzling AXNODR challenge.cpp
@@ -0,0 +1,32 @@
+/* Approach --> Observation*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<ll>
+#define mod 1000000007
+#define f0 for(int i=0;i<n;i++)
+#define mi map<ll,ll>
+
+void baj(){
+    ll n;
+    cin>>n;
+    if(n%4==2||n%4==3) cout<<3<<endl;
+    else if(n%4==0){
+        cout<<n+3<<endl;
+    }
+    else cout<<n<<endl;
+}
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--){
+	  baj();
+	}
+	return 0;
+}
