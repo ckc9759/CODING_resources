4a23ca3877b12daf9775d740153628ca7a3071cc
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-07-09 16:00:36 +0530

    Create And plus xor.cpp

---
+++ Codechef/Bit Manipulation/And plus xor.cpp
@@ -0,0 +1,28 @@
+/*Approach --> This is a simple problem using xor properties, a and 0 is zero while a or 0 is a */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<ll>
+#define mod 1000000007
+#define f0 for(int i=0;i<n;i++)
+#define mi map<ll,ll>
+
+void baj(){
+    ll x;
+    cin>>x;
+    cout<<x<<" "<<0<<endl;
+}
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--){
+	  baj();
+	}
+	return 0;
+}
