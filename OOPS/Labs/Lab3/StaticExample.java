740a33ca67089e8d14861acb2385ba565b20dc3c
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-09-20 15:48:15 +0530

    Create StaticExample.java

---
+++ OOPS/Labs/Lab3/StaticExample.java
@@ -0,0 +1,32 @@
+package Lab3;
+
+public class StaticExample {
+	static {
+		System.out.println("This is first static block"); }
+	
+		public StaticExample(){
+		System.out.println("This is constructor");
+		}
+		
+		public static String staticString = "Static Variable";
+		
+		static {
+		System.out.println("This is second static block and " +
+		staticString); }
+		
+		public static void main(String[] args){
+		StaticExample statEx = new StaticExample();
+		StaticExample.staticMethod2();
+		}
+		
+		static {
+		staticMethod();
+		System.out.println("This is third static block"); }
+		
+		public static void staticMethod() {
+		System.out.println("This is static method"); }
+		
+		public static void staticMethod2() {
+		System.out.println("This is static method2"); }
+		
+}
