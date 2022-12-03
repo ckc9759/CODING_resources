7a6a096dd21b560769dd84ea45f2a7e995d6bd23
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-03 16:58:53 +0530

    Update and rename readme.md to readme.py

--- CP/readme.py
+++ CP/readme.py
@@ -1,6 +1,5 @@
-
 #include <stdio.h>
-
+#include <stdlib.h>
 typedef struct node * NODE;
 struct node{
 	int ele;
@@ -9,6 +8,7 @@ struct node{
 
 typedef struct linked_list * LIST;
 struct linked_list{
+	int count;
 	char ID[30];
 	char Name[50];
 	char Dept[50];
@@ -22,11 +22,26 @@ LIST createNewList(){
 	LIST myList;
 	myList = (LIST) malloc(sizeof(struct linked_list));
 // myList = (LIST) malloc(sizeof(*myList));
-	myList->ID=NULL;
-	myList->Name=NULL;
-	myList->Dept=NULL;
+	myList->count=0;
+	myList->head=NULL;
 	myList->math_marks=0;
 	myList->phy_marks=0;
 	myList->chem_marks=0;
 	return myList;
+}
+
+void printList(LIST l1){
+	if(l1->count==0){
+		printf("List is empty !! Nothing to print\n");
+	}
+	NODE temp=l1->head;
+	while(temp!=NULL){
+		printf("%d\n",temp->Name);
+		printf("%d\n",temp->ID);
+		printf("%d\n",temp->Dept);
+		printf("%d\n",temp->math_marks);
+		printf("%d\n",temp->phy_marks);
+		printf("%d\n",temp->chem_marks);
+		temp=temp->next;
+	}
 }
