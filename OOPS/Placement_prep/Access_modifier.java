41083455abd2f12fae56d78bf233913164959f3b
Chaitanya Krishna Chauhan <95117634+ckc9759@users.noreply.github.com>
2023-12-27 12:04:38 +0530

    Create Access_modifier.java

---
+++ OOPS/Placement_prep/Access_modifier.java
@@ -0,0 +1,16 @@
+package bank;
+
+class Account{
+    public String name;
+}
+
+public class bank {
+    
+}
+
+// public, private,protected,default
+// public -> globally accesible in any class and package once imported
+// default -> already exists if no other access modifier is used, accesible in the same package
+// protected -> accesible only in subclasses of different pacakage
+// private -> accesible only in the same class
+// getters and setters -> to access private members indirectly
